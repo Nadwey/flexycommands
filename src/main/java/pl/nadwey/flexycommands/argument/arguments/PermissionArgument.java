70c8c872a6810d1f35c0cdaf72cc72d30bdb06a1
Nadwey <nadweyyy@gmail.com>
2024-07-05 18:51:19 +0200

    Take PluginManager instead of JavaPlugin in PermissionArgument

--- src/main/java/pl/nadwey/flexycommands/argument/arguments/PermissionArgument.java
+++ src/main/java/pl/nadwey/flexycommands/argument/arguments/PermissionArgument.java
@@ -3,30 +3,29 @@ package pl.nadwey.flexycommands.argument.arguments;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.bukkit.permissions.Permission;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import pl.nadwey.flexycommands.SuggestionContext;
 import pl.nadwey.flexycommands.argument.SuggestionResult;
 
 public class PermissionArgument extends StringArgument {
 
-	private final JavaPlugin plugin;
+	private final PluginManager pluginManager;
 
-	public PermissionArgument(String name, JavaPlugin plugin) {
+	public PermissionArgument(String name, PluginManager pluginManager) {
 		super(name);
-		this.plugin = plugin;
+		this.pluginManager = pluginManager;
 	}
 
 	@Override
 	public SuggestionResult suggest(SuggestionContext context, String value) {
 		StringArgumentParseResult result = parse(value);
 
-		List<String> permissions = plugin
-			.getServer()
-			.getPluginManager()
+		List<String> permissions = pluginManager
 			.getPermissions()
 			.stream()
 			.map(Permission::getName)
-			.filter(permission -> permission.startsWith((String) result.value))
+			.filter(permission -> permission.startsWith(result.value))
 			.collect(Collectors.toList());
 
 		return new SuggestionResult(
